cbd10c590173b19a718d75d14cfa2795471cade3
Alonso Gil <alonsogil117@gmail.com>
2022-01-21 16:15:57 -0600

    Carrusel de marcas agregado

--- cliente/components/ui/CarruselMarcas.js
+++ cliente/components/ui/CarruselMarcas.js
@@ -1,10 +1,81 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { Image } from "semantic-ui-react";
 
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      paritialVisibilityGutter: 60
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      paritialVisibilityGutter: 50
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      paritialVisibilityGutter: 30
+    }
+  };
 
+  const images = [
+    {
+        id: 1,
+        url: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 3,
+        url: "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 4,
+        url: "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 5,
+        url: "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 6,
+        url: "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 7,
+        url: "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 8,
+        url: "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+  ]
 
 const CarruselMarcas = () => {
     return ( 
-        <h1>XD</h1>
+        <Carousel
+            ssr
+            itemClass="image-item"
+            responsive={responsive}
+            autoPlay={true}
+            infinite={true}
+        >
+            {images.slice(0, 5).map(image => {
+                return (
+                    <Image
+                        draggable={false}
+                        style={{ width: "95%", height: "95%" }}
+                        src={image.url}
+                        key={image.id}
+                    />
+                );
+            })}
+        </Carousel>
      );
 }
  
